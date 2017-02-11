eeea8b5fb2394c3b3d1d0d66fe85ef7b6195b3c7
wkisielewicz <wkisielewicz@criticue.com>
2017-02-11 15:40:06 +0100

    Chapter 3, filters.

---
+++ chapter_3/assets/js/controller.js
@@ -0,0 +1,32 @@
+angular.module("app", []).controller("AppCtrl", function($scope) {
+    $scope.clickHandler = function() {
+        //window.alert('Kliknięty')
+        $scope.isHidden = !$scope.isHidden;
+    }
+
+
+    $scope.contacts = [{
+            name: 'Janko Walski',
+            phone: '01234567890',
+            email: 'janko@walski.com'
+        },
+        {
+            name: 'Karen Nerka',
+            phone: '09876543210',
+            email: 'karenne@email.com'
+        }
+    ];
+
+
+    $scope.styleDemo = function() {
+        if (!$scope.styler) {
+            return;
+        }
+
+        return {
+            background: 'red',
+            fontWeight: 'bold'
+        };
+    };
+
+});
